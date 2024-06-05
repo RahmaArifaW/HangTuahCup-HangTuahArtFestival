ef09ead2fc20e38fda5c4a6d1e8047e22b61750d
RahmaArifaW <rahma.arifa2020@gmail.com>
2024-06-06 00:07:14 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const timeLeft = document.getElementById('time-left')
+const button1 = document.getElementById('button1')
+
+//month day year
+const duedate = new Date('June 05, 2024 23:00:00')
+
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+let timerId
+var timer;
+var ele = document.getElementById('timer')
+
+function timer() {
+  var sec = 0;
+  timer = setInterval(() => {
+    ele.innerHTML = '00: ' + sec;
+    sec++;
+  }, 1000)
+
+}
+
+function bukain() {
+  window.open("https://bit.ly/PengumumanHasilSeleksiPengurusMPKOSIS2023_HT1")
+}
+
+function countDown() {
+  const today = new Date()
+  const timeSpan = duedate - today
+  //milliseconds
+  console.log(timeSpan)
+
+  if (timeSpan <= -day) {
+    timeLeft.innerHTML = 'Semoga Menerima Apapun Hasilnya!'
+    clearInterval(timerId)
+    return
+  }
+
+  if (timeSpan <= 0) {
+    timeLeft.innerHTML = 'Silahkan Klik Tombol Di Bawah Ini <br> <button onclick="bukain()">KLIK DISINI</button>'
+    clearInterval(timerId)
+    return
+  }
+
+
+  const days = Math.floor(timeSpan / day)
+  const hours = Math.floor((timeSpan % day) / hour)
+  const minutes = Math.floor((timeSpan % hour) / minute)
+  const seconds = Math.floor((timeSpan % minute) / second)
+
+  timeLeft.innerHTML =
+    days + 'Hari ' + hours + 'Jam ' + minutes + 'Menit ' + seconds + 'Detik '
+}
+
+timerId = setInterval(countDown, second)
