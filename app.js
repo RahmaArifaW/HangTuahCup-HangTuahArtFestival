e66bc742cee3ab3ed76c961e649d75c89c740586
RahmaArifaW <rahma.arifa2020@gmail.com>
2024-06-06 09:50:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const timeLeft = document.getElementById('time-left')
 const button1 = document.getElementById('button1')
 
 //month day year
-const duedate = new Date('June 06, 2024 13:15:00')
+const duedate = new Date('June 06, 2024 09:55:00')
 
 const second = 1000
 const minute = second * 60
